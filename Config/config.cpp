2db08a4a2c33860bedd56dfb486556d130257963
Robin <Geral.robin@yahoo.fr>
2023-04-05 16:58:53 +0200

    rework parsing

--- Config/config.cpp
+++ Config/config.cpp
@@ -1,168 +1,56 @@
 #include "config.hpp"
-
-Config::Config()
+Token::Token(std::string token, type tokentype) : _Token(token), _tokenType(tokentype)
 {
-    std::cout << "config on\n";
+    std::cout << "Token creation\n";
 }
-
-Server::Server(std::vector<std::string> content) : _serverconfig(content)
-{
-    std::cout << "server creation\n";
-    /*for (size_t i = 0; i < _servconfig.size(); i++)
-    {
-        std::cout << _servconfig[i];
-    }*/
+Token::~Token() {}
+std::string Token::getType() {
+    static const char* typeNames[] = {"ident", "openbracket", "closebracket", "semicolon", "string"};
+    return typeNames[_tokenType];
 }
-
-Config::~Config() {
-    for (size_t i = 0; i < _servconfig.size(); i++) {
-        delete _servconfig[i];
-    }
-    std::cout << "config destructor\n";
-}
-
-Server::~Server() {
-    std::cout << "server destructor\n";
-}
-
-std::vector<std::string> Config::getConfig() {return(_config);}
-std::vector<std::string> Server::getServConfig() {return(_serverconfig);}
-std::string Server::getServConfigIndex(size_t index) {return(_serverconfig[index]);}
-
-std::vector<Server *> Config::getServConfig() {return(_servconfig);}
-
-std::vector<std::string>	split_with_space(std::string line)
-{
-    std::vector<std::string>	words;
-
-    if (line.size() == 0)
-        return (words);
-    int k = 0;
-    int j = -1;
-    for (int i = 0; i < line.size(); i++)
-    {
-        if (line[i] == ' ' || line[i] == '\t')
-        {
-            if (i != 0)
-                k++;
-            while (line[i] == ' ' || line[i] == '\t')
-                i++;
-            i--;
-        }
-        else
-        {
-            if (j != k)
-            {
-                j = k;
-                words.push_back("");
-            }
-            words[k].push_back(line[i]);
-        }
-    }
-    return (words);
-}
-
-std::vector<std::string> Config::tokenization(std::string line, std::vector<std::string> &conf)
-{
-    std::vector<std::string> clean;
-    std::vector<std::string> tmp;
-
-    size_t is_title = line.find('{');
-    if (is_title != std::string::npos)
-    {
-        std::size_t first_non_space = line.find_first_not_of(" /t/n");
-        std::string title_line = line.substr(first_non_space, line.length() - 1 - first_non_space);
-        conf.push_back("Title : " + title_line);
-        conf.push_back("Open bracket : {");
-        conf.push_back("End of line \n");
-        return(conf);
-    }
-    tmp = split_with_space(line);
-    for (size_t i = 0; i < tmp.size(); i++)
-    {
-        if (tmp[i] == "{")
-            conf.push_back("Open bracket : " + tmp[i]);
-        else if (tmp[i] == "}")
-            conf.push_back("Close bracket : " + tmp[i]);
-        else
-            conf.push_back("Text : " + tmp[i]);
-    }
-    conf.push_back("End of line \n");
-    return(conf);
-}
-
-void Config::makeConfig()
-{
-    std::string filename = "../config.conf";
-    std::ifstream file(filename.c_str());
-    std::vector<std::string> final;
-
-    if (file.good())
-    {
-        int it;
-        std::string line;
-        while (std::getline(file, line, '\n')) {
-            if (line.size() != 0)
-                Config::tokenization(line, final);
-        }
-        file.close();
-    }
-    else
-        exit(1);
-    final.push_back("End of file\n");
-    _config = final;
-}
-
-void Config::split_serv(std::vector<std::string> args)
-{
-    std::vector<std::string> final;
-    size_t i = 0;
-
-    while (i < args.size())
-    {
-        size_t title = args[i].find("Title : server");
-        if (title != std::string::npos)
-        {
-            final.push_back(args[i]);
-            i++;
-            size_t split = args[i].find("Title : server");
-            while (i < args.size())
-            {
-                split = args[i].find("Title : server");
-                if (split != std::string::npos)
-                    break;
-                final.push_back(args[i]);
-                i++;
-            }
-            //_servconfig.push_back(final);
-            Server* new_server = new Server(final);
-            _servconfig.push_back(new_server);
-            //std::cout << "End of Server\n\n";
-            final.clear();
-        }
-        else
-            i++;
-    }
-    //for (size_t k = 0; k < _test[0].)
-}
+std::string Token::getToken() {return (_Token);}
 
 int main()
 {
-    std::vector<std::string> infile;
-    std::vector<std::string> final;
-    std::vector<Server*> servers;
-    Config config;
-    config.makeConfig();
-    final = config.getConfig();
-    config.split_serv(final);
-    servers = config.getServConfig();
-    for (size_t i = 0; i < servers.size(); i++)
-{
-        for (size_t j = 0; j < servers[i]->getServConfig().size(); j++)
+    char ch;
+    std::vector<Token *> tokens;
+    std::string line;
+    std::string filename = "../config.conf";
+    std::ifstream file(filename.c_str());
+
+    while (file.get(ch))
+    {
+        std::cout << ch << std::endl;
+        if (ch == ';')
         {
-            std::cout << servers[i]->getServConfigIndex(j);
+            if (line.size() > 1)
+            {
+                file.unget();
+                std::cout << "1";
+                tokens.push_back(new Token(line, ident));
+                line.clear();
+            }
+            else
+            {
+                std::cout << "2";
+                tokens.push_back(new Token(";", semicolon));
+                file.get();
+                line.clear();
+            }
         }
-        std::cout << "END SERV\n\n";
-}
-    //config conf(infile);
+        else if (isspace(ch))
+        {
+            std::cout << "3";
+            tokens.push_back(new Token(line, ident));
+            line.clear();
+            while (isspace(ch))
+                file.get(ch);
+        }
+        if (ch != ';')
+            line += ch;
+    }
+    for (size_t i = 0; i < tokens.size(); i++)
+    {
+        std::cout << "Type : "<< tokens[i]->getType() << ", Value : "<< tokens[i]->getToken() << std::endl;
+    }
 }
--- Config/config.hpp
+++ Config/config.hpp
@@ -9,39 +9,25 @@
 #include <algorithm>
 #include <cstring>
 #include <cctype>
+#include <fstream>
 
-class Server {
+
+enum type {
+    ident,
+    openbracket,
+    closebracket,
+    semicolon,
+    string,
+};
+class Token {
 public:
-    Server(std::vector<std::string> content);
-    ~Server();
-    std::vector<std::string> getServConfig();
-    std::string getServConfigIndex(size_t index);
+    Token(std::string token, type tokentype);
+    ~Token();
+    std::string getType();
+    std::string getToken();
+
 
 private:
-    std::vector<std::string> _serverconfig;
+    type _tokenType;
+    std::string _Token;
 };
-
-class Config {
-
-public :
-    //config(std::vector<std::string> conf);
-    Config();
-    ~Config();
-    std::vector<std::string> getConfig();
-    std::vector<Server *> getServConfig();
-
-    /*Tokenization process make _config*/
-    void makeConfig();
-    std::vector<std::string> tokenization(std::string line, std::vector<std::string> &conf);
-    void split_serv(std::vector<std::string> args);
-
-    class InvalidConfigFile : public std::exception {
-    public :
-        virtual const char* what() const throw() {return "invalid configfile";}
-    };
-
-private:
-    std::vector<std::string> _config;
-    std::vector<Server *> _servconfig;
-};
-
