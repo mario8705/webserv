8ad4bf7e8ab333d6bf46c3d96b9a40b9f0cfd532
Robin <Geral.robin@yahoo.fr>
2023-04-14 19:54:41 +0200

    rules v1 working

--- Config/config.cpp
+++ Config/config.cpp
@@ -1,4 +1,6 @@
 #include "config.hpp"
+
+/*          ----- TOKEN CLASS ----              */
 Token::Token(std::string token, TokenType tokentype) : _Token(token), _tokenType(tokentype)
 {
     //std::cout << "Token creation\n";
@@ -11,10 +13,10 @@ std::string Token::getTypeString() {
     static const char* typeNames[] = {"ident", "left bracket", "right bracket", "semicolon", "string"};
     return typeNames[_tokenType];
 }
-int main()
+
+void Token::tokenization(std::vector<Token *> *tokens)
 {
     char ch;
-    std::vector<Token *> tokens;
     std::string line;
     std::string stringline;
     std::string filename = "../config.conf";
@@ -25,7 +27,7 @@ int main()
         if (isspace(ch))
         {
             if (!line.empty())
-                tokens.push_back(new Token(line, kTokentype_Ident));
+                tokens->push_back(new Token(line, kTokenType_Ident));
             //tokens.push_back(new Token(line, ident));
             line.clear();
             while(isspace(ch) && !file.eof())
@@ -34,9 +36,9 @@ int main()
         if (ch == ';')
         {
             if (!line.empty())
-                tokens.push_back(new Token(line, kTokentype_Ident));
+                tokens->push_back(new Token(line, kTokenType_Ident));
             line.clear();
-            tokens.push_back(new Token(";", kTokentype_Semicolon));
+            tokens->push_back(new Token(";", kTokenType_Semicolon));
             file.get(ch);
             while(isspace(ch) && !file.eof())
             {
@@ -52,14 +54,16 @@ int main()
         }
         if (ch == '{')
         {
-            tokens.push_back(new Token("{", kTokentype_Leftbracket));
+            if (!line.empty())
+                tokens->push_back(new Token(line, kTokenType_Ident));
+            tokens->push_back(new Token("{", kTokenType_Leftbracket));
             line.clear();
             while(isspace(ch) && !file.eof())
                 file.get(ch);
         }
         if (ch == '}')
         {
-            tokens.push_back(new Token("}", kTokentype_Rightbracket));
+            tokens->push_back(new Token("}", kTokenType_Rightbracket));
             line.clear();
             while(isspace(ch) && !file.eof())
                 file.get(ch);
@@ -74,7 +78,7 @@ int main()
                 file.get(ch);
             }
             stringline += ch;
-            tokens.push_back(new Token(stringline, kTokentype_String));
+            tokens->push_back(new Token(stringline, kTokenType_String));
             stringline.clear();
             while(isspace(ch) && !file.eof())
                 file.get(ch);
@@ -82,8 +86,88 @@ int main()
         if (ch != ';' && ch != '\n' && ch != '#' && ch!= '\'' && ch != '{' && ch != '}' && !file.eof())
             line += ch;
     }
+}
+
+/*          ----- CONFIG CLASS ----              */
+
+ConfigProperty::ConfigProperty(std::vector<std::string> Tokens) : _params(Tokens)
+{
+    //std::cout << "Config constructor" << std::endl;
+}
+ConfigProperty::~ConfigProperty() {}
+
+std::vector<std::string> ConfigProperty::getParams() {
+    return(_params);
+}
+
+void ConfigProperty::add_body(ConfigProperty *value) {
+    _body.push_back(value);
+}
+
+const std::vector<ConfigProperty *> &ConfigProperty::getBodystring() {
+    return (_body);
+}
+
+void ConfigProperty::push_config(std::vector<Token *> tokens, std::vector<ConfigProperty *> *config) {
+    std::vector<std::string> tmp;
+    std::stack<ConfigProperty *> stck;
     for (size_t i = 0; i < tokens.size(); i++)
     {
-        std::cout << "Token "<< tokens[i]->getTypeString() << ", value: "<< tokens[i]->getToken() << std::endl;
+        if (tokens[i]->getTypeString() != "left bracket" && tokens[i]->getTypeString() != "right bracket")
+            tmp.push_back(tokens[i]->getToken());
+        if (tokens[i]->getTypeString() == "left bracket") {
+            stck.push(new ConfigProperty(tmp));
+            config->push_back(stck.top());
+            tmp.clear();
+        }
+        else if (tokens[i]->getTypeString() == "right bracket")
+            stck.pop();
+        if (tokens[i]->getTypeString() == "semicolon") {
+            if (!stck.empty()) {
+               /* for (size_t i = 0; i < tmp.size(); i++) {
+                    std::cout << tmp[i];
+                }
+                std::cout << " RULE OF :: ";
+                std::vector<std::string> tmp2 = stck.top()->getParams();
+                for (size_t i = 0; i < tmp2.size(); i++) {
+                    std::cout << tmp2[i];
+                }
+                //std::cout << std::endl;*/
+                stck.top()->add_body(new ConfigProperty(tmp));
+            } else
+                config->push_back(new ConfigProperty(tmp));
+            tmp.clear();
+        }
+    }
+}
+
+#include <stack>
+
+int main()
+{
+    std::vector<Token *> tokens;
+    std::vector<ConfigProperty *> configs;
+    Token::tokenization(&tokens);
+    ConfigProperty::push_config(tokens, &configs);
+    std:: cout << "---------------\nCONFIGS :: \n\n";
+    for (size_t i = 0; i < configs.size(); i++ )
+    {
+        std::vector<std::string> tmp = configs[i]->getParams();
+        std::vector<ConfigProperty *> tmp2 = configs[i]->getBodystring();
+        for (size_t j = 0; j < tmp.size(); j++)
+        {
+            std::cout << "CONFIG :: "<< tmp[j] << std::endl;
+            for (size_t k = 0; k < tmp2.size(); k++)
+            {
+                std::vector<std::string> tmp3 = tmp2[k]->getParams();
+                std::cout << "Rule of :: "<< tmp[j] << " :: ";
+                for (size_t l = 0; l < tmp3.size(); l++)
+                {
+                    std::cout << tmp3[l];
+                }
+                std::cout << std::endl;
+            }
+
+        }
     }
 }
--- Config/config.hpp
+++ Config/config.hpp
@@ -10,15 +10,18 @@
 #include <cstring>
 #include <cctype>
 #include <fstream>
+#include <stack>
 
 
 enum TokenType {
-    kTokentype_Ident,
-    kTokentype_Leftbracket,
-    kTokentype_Rightbracket,
-    kTokentype_Semicolon,
-    kTokentype_String,
+    kTokenType_Ident,
+    kTokenType_Leftbracket,
+    kTokenType_Rightbracket,
+    kTokenType_Semicolon,
+    kTokenType_String,
 };
+
+
 class Token {
 public:
     Token(std::string token, TokenType tokentype);
@@ -26,10 +29,25 @@ public:
     TokenType getType();
     std::string getTypeString();
     std::string getToken();
+    static void tokenization(std::vector<Token *>*tokens);
 
 
 
 private:
     TokenType _tokenType;
     std::string _Token;
+};
+class ConfigProperty
+{
+public:
+    ConfigProperty(std::vector<std::string>Tokens);
+    ~ConfigProperty();
+    static void push_config(std::vector<Token *> tokens, std::vector<ConfigProperty *> *config);
+    void add_body(ConfigProperty *value);
+    const std::vector<ConfigProperty *> &getBodystring();
+    std::vector<std::string> getParams();
+private:
+    std::vector<std::string> _params;
+    std::vector<ConfigProperty *> _body;
+
 };
