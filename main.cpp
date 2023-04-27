56ef8ae2e7a1948f750cd058979dc5ae16ea8052
Alexis Lavaud <alexislavaud191@gmail.com>
2023-04-27 19:18:29 +0200

    Cleaning main (trying to trigger build hook)

--- main.cpp
+++ main.cpp
@@ -8,18 +8,9 @@ static void handler_stub(int)
     s_instance->Stop();
 }
 
-#include "Http/URL.h"
-
 int main(int argc, char *argv[])
 {
     Webserv webserv;
-
-    /*
-    URL::Decode("Hello%20World%");
-    URL::Decode("Hello%20World%2");
-    URL::ParseURL("/test/resource/123%20 Hello-lol?raw=true&test=bonjour");
-    return 0;
-     */
 
     s_instance = &webserv;
 
