02ce61b193ac8ca7db59012120103d513e863ae7
Alexis Lavaud <alexislavaud191@gmail.com>
2023-05-07 16:46:08 +0200

    Logger testing

--- Logger/Logger.cpp
+++ Logger/Logger.cpp
@@ -4,60 +4,11 @@
 
 #include "Logger.hpp"
 
-Logger::Logger(){
-	logFile.open("logfile.txt", std::ios_base::app);
-}
-
-Logger::Logger(const Logger &copy)
+ILogger::~ILogger()
 {
-	*this = copy;
 }
 
-Logger::~Logger() {
-	if (logFile.is_open()) {
-		logFile.close();
-	}
-}
-
-Logger &Logger::operator=(const Logger &toAssign)
+std::ostream &ILogger::Log(LogLevel level)
 {
-	(void)toAssign;
-	return *this;
-}
-
-void Logger::log(int level) {
-    log(level, "");
-}
-
-void Logger::log(int level, const std::string &message) {
-	time_t now = std::time(NULL);
-	char timestamp[20];
-	std::strftime(timestamp, sizeof(timestamp), "%Y-%m-%d %H:%M:%S", std::localtime(&now));
-	std::string ip = "[PH]USER IP";
-	std::string method = "[PH]METHOD";
-	int errorCode = 420;
-	std::string navInfos = "[PH]Navigator Infos";
-
-	std::string severityColor;
-	switch (level) {
-		case DEBUG: severityColor = Blue; break;
-		case INFO: severityColor = Green; break;
-		case WARNING: severityColor = Yellow; break;
-		case ERROR: severityColor = Red; break;
-		default: severityColor = Reset;
-	}
-
-
-	switch (level) { //En remplaçant std::cout par logFile, les infos seront écrites dans un fichier (mais les couleurs seront perdues)
-		case DEBUG: std::cout << severityColor << ip + " - - [" << timestamp << "] ";
-		case INFO: std::cout << severityColor << "\"" + method + "\" ";
-		case WARNING: std::cout << severityColor << errorCode << " ";
-		case ERROR: std::cout << severityColor << navInfos << Reset; break;
-        default: std::cerr << "[LOGGER] Wrong log level given\n";
-	}
-    std::cout << ": " + message + "\n";
-}
-
-void Logger::setOutput(std::ostream &out) {
-
+    return GetFacility(level).Begin();
 }
--- Logger/Logger.hpp
+++ Logger/Logger.hpp
@@ -8,39 +8,95 @@
 #include <iostream>
 #include <ctime>
 #include <fstream>
+#include <sstream>
 
-#define Black "\u001b[30m"
-#define Red "\u001b[31m"
-#define Green "\u001b[32m"
-#define Yellow "\u001b[33m"
-#define Blue "\u001b[34m"
-#define Magenta "\u001b[35m"
-#define Cyan "\u001b[36m"
-#define Reset "\u001b[37m"
-
-enum e_level{
-    DEBUG,
-    INFO,
-    WARNING,
-    ERROR
+enum AnsiColor
+{
+    kAnsiColor_Black = 0,
+    kAnsiColor_Red,
+    kAnsiColor_Green,
+    kAnsiColor_Yellow,
+    kAnsiColor_Blue,
+    kAnsiColor_Magenta,
+    kAnsiColor_Cyan,
+    kAnsiColor_Reset,
 };
 
-class Logger
+enum LogLevel
 {
-private:
-	std::ofstream logFile;
+    kLogLevel_Debug,
+    kLogLevel_Info,
+    kLogLevel_Warning,
+    kLogLevel_Error,
+};
+
+class ILogger;
+
+class LoggerFacility : public std::ostream
+{
+    friend class ILogger;
 
 public:
-	Logger();
-	Logger(const Logger &copy);
-	~Logger();
-	Logger &operator=(const Logger &toAssign);
+    LoggerFacility(std::streambuf *buf, LogLevel level, bool ansiColors)
+        : std::ostream(buf), m_level(level), m_ansiColors(ansiColors)
+    {
+    }
 
+    ~LoggerFacility()
+    {
+    }
 
-	void log(int level);
-	void log(int level, const std::string &message);
-    void setOutput(std::ostream &out);
+    template <typename T>
+    LoggerFacility &operator<<(const T &value)
+    {
+        return *this;
+    }
 
+    bool HasColors() const
+    {
+        return m_ansiColors;
+    }
+
+    LoggerFacility &PutColor(const AnsiColor &color)
+    {
+        if (m_ansiColors) {
+            *this << "\x1b[3" << (char) ('0' + (int) color) << "m";
+        }
+        return *this;
+    }
+
+private:
+    LoggerFacility &Begin()
+    {
+        PutColor(kAnsiColor_Blue);
+        *this << "[LEVEL HERE] ";
+        return *this;
+    }
+
+    LogLevel m_level;
+    bool m_ansiColors;
+};
+
+inline std::ostream &operator<<(std::ostream &os, const AnsiColor &color)
+{
+    LoggerFacility *f;
+
+    if ((f = dynamic_cast<LoggerFacility *>(&os)))
+    {
+        return f->PutColor(color);
+    }
+    return os;
+}
+
+class ILogger
+{
+public:
+	virtual ~ILogger();
+
+    virtual std::ostream &Log(LogLevel level);
+
+protected:
+    virtual LoggerFacility &GetFacility(LogLevel level) = 0;
 };
 
 
